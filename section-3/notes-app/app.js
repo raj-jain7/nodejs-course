08d543cda45131bbd57449cbbe1f2aac9cb877ae
Raj Jain <rajjain@gb3crem0q0pvg.dyn.oneds.com>
2023-03-06 18:06:17 +0000

    using npm package manager

--- section-3/notes-app/app.js
+++ section-3/notes-app/app.js
@@ -1,5 +1,9 @@
 const adding = require("./utils.js");
 const message = require("./notes.js");
+const validator = require("validator");
 const sum = adding(300, 400);
 console.log(sum);
 console.log(message());
+if (validator.isEmail("rajkjain04@gmail.com") === true) {
+  console.log("Your email is correct");
+}
